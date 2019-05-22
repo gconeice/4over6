8b276e78091bbfd5eaa5560cb19ff21b72ab09c7
gconeice <421662885@qq.com>
2019-05-22 20:00:08 +0800

    server version 1 finish

--- server/main.cpp
+++ server/main.cpp
@@ -3,11 +3,121 @@
 
 using namespace std;
 
-static struct IPADDR ipaddr[CLIENT_NUM]; //全局变量的地址池
-struct User_Info_Table users[CLIENT_NUM];
-struct buffer_entry bufs[MAX_FDS];
+static struct User_Info_Table users[CLIENT_NUM];
+static struct buffer_entry bufs[MAX_FDS];
 static struct sockaddr_in6 server_addr, client_addr;
 int server_fd = -1, tun_fd = -1, epoll_fd = -1;
+
+// search for user info entry given ipv4 address
+int search_user_info_by_addr(uint32_t addr)
+{
+    int i;
+    for(i = 0; i < CLIENT_NUM; i ++)
+        if (users[i].v4addr.s_addr == addr && !users[i].status)
+            return i;
+    return -1;
+}
+
+// search for user info entry given fd
+int search_user_info_by_fd(int client_fd)
+{
+	int i;
+	for (i = 0; i < CLIENT_NUM; i ++)
+		if (users[i].fd == client_fd && !users[i].status)
+			return i;	
+	return -1;
+}
+
+int epoll_add_fd(int epoll_fd, int fd, int events)
+{
+    // add socket to epoll
+    struct epoll_event ev;
+    ev.data.fd = fd;
+    ev.events = events;
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) < 0)
+	{
+        printf("Error: failed to add fd %d to epoll\n", fd);
+        return -1;
+    }
+    return 0;
+}
+
+// remove fd from epoll
+int epoll_remove_fd(int epoll_fd, int fd)
+{
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, fd, NULL) < 0)
+	{
+        printf("Error: failed to remove fd %d from epoll\n", fd);
+        return -1;
+    }
+    return 0;
+}
+
+// allocate ip address from pool
+int allocate_ip_addr(int client_fd)
+{
+    int i;
+    for(i = 0; i < CLIENT_NUM; i ++)
+        if (users[i].status)
+		{
+            users[i].status = 0;
+
+            users[i].fd = client_fd;
+            users[i].last_heartbeat_sent_secs = time(0);
+            users[i].last_heartbeat_recved_secs = time(0);
+            inet_pton(AF_INET, users[i].addr, (void *)&users[i].v4addr);
+
+            struct sockaddr_in6 clientaddr;
+            socklen_t addrsize = sizeof(clientaddr);
+            getpeername(client_fd, (struct sockaddr *)&clientaddr, &addrsize);
+            users[i].v6addr = clientaddr.sin6_addr;
+            
+            return i;
+        }
+    return -1;
+}
+
+// deallocate ip address from pool
+int deallocate_ip_addr(int client_fd)
+{
+    int i;
+    for(i = 0; i < CLIENT_NUM; i ++)
+        if (users[i].fd == client_fd && !users[i].status)
+		{
+            users[i].status = 1;
+            users[i].fd = -1;
+            return i;
+        }
+    return -1;
+}
+
+void free_client_fd(int client_fd, int epoll_fd)
+{
+    if (epoll_remove_fd(epoll_fd, client_fd) < 0)
+	{
+        printf("Error: failed to remove client fd %d from epoll\n", client_fd);
+        return;
+    }
+    deallocate_ip_addr(client_fd);
+    close(client_fd);
+}
+
+int write_all(int fd, void* buf, int size)
+{
+    int offset = 0;
+    uint8_t* w = (uint8_t*)buf;
+    while(offset < size)
+	{
+        int ret;
+        if ((ret = write(fd, w+offset, size-offset)) < 0)
+		{
+            printf("Error: fail to write data to fd = %d: %s\n", fd, strerror(errno));
+            return ret;
+        }
+        offset += ret;
+    }
+    return offset;
+}
 
 // socket 初始化
 int ServerInit()
@@ -52,7 +162,8 @@ int TunInit() {
     int fd;
 
     // open tun
-    if ((fd = open("/dev/net/tun", O_RDWR)) < 0) {
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
+	{
         printf("Error: open /dev/net/tun failed\n");
         return -1;
     }
@@ -63,7 +174,8 @@ int TunInit() {
     ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
     strncpy(ifr.ifr_name, "over6", IFNAMSIZ);
 
-    if (ioctl(fd, TUNSETIFF, (void *)&ifr) == -1) {
+    if (ioctl(fd, TUNSETIFF, (void *)&ifr) == -1)
+	{
         printf("Error: set tun interface name failed\n");
         return -1;
     }
@@ -73,18 +185,6 @@ int TunInit() {
     system("iptables -t nat -A POSTROUTING -s " ADDRESS_PREFIX ".0/24 -j MASQUERADE");
 
     return fd;
-}
-
-int epoll_add_fd(int epoll_fd, int fd, int events) {
-    // add socket to epoll
-    struct epoll_event ev;
-    ev.data.fd = fd;
-    ev.events = events;
-    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd, &ev) < 0) {
-        printf("Error: failed to add fd %d to epoll\n", fd);
-        return -1;
-    }
-    return 0;
 }
 
 int MainThread()
@@ -98,23 +198,27 @@ int MainThread()
 	printf("tun_fd = %d\n", tun_fd);
 
 	// 初始化epoll
-    if ((epoll_fd = epoll_create(EPOLL_MAX_EVENTS)) < 0) {
+    if ((epoll_fd = epoll_create(EPOLL_MAX_EVENTS)) < 0)
+	{
         printf("Error: create epoll fd failed\n");
 		return -1;
     }
 
 	// 加入server和tun
-	if (epoll_add_fd(epoll_fd, server_fd, EPOLL_EVENTS) < 0) {
+	if (epoll_add_fd(epoll_fd, server_fd, EPOLL_EVENTS) < 0)
+	{
         printf("Error: failed to add server socket to epoll\n");
 		return -1;
     }
-    if (epoll_add_fd(epoll_fd, tun_fd, EPOLL_EVENTS) < 0) {
+    if (epoll_add_fd(epoll_fd, tun_fd, EPOLL_EVENTS) < 0)
+	{
         printf("Error: failed to add tun to epoll\n");
 		return -1;
     }
 
 	// 初始化user table
-    for (int i = 0; i < CLIENT_NUM; i++) {
+    for (int i = 0; i < CLIENT_NUM; i++)
+	{
         sprintf(users[i].addr, ADDRESS_PREFIX ".%d", i);
         users[i].status = 1;
         users[i].fd = -1;
@@ -124,19 +228,243 @@ int MainThread()
     users[255].status = 0; // x.x.x.255 is invalid
 
 	// 初始化buffer pool
-    for (int i = 0; i < MAX_FDS; i++) {
+    for (int i = 0; i < MAX_FDS; i++)
+	{
         bufs[i].used = 0;
     }
 
 	return 0;
 }
 
-int main()
+void process_server(int server_fd, int epoll_fd)
 {
-	if (MainThread() == -1) goto clean;
-	while (1)
+    struct sockaddr_in6 clientaddr;
+    memset(&clientaddr, 0, sizeof(clientaddr));
+    socklen_t addrsize = sizeof(clientaddr);
+
+    // accept new connection from a client
+    int client_fd;
+    if ((client_fd = accept(server_fd, (struct sockaddr *)&clientaddr, &addrsize)) < 0)
 	{
-		cout << "cnm" << endl;
+        printf("Error: accept connection failed\n");
+        return;
+    }
+
+    // setup buffer
+    bufs[client_fd].used = 0;
+
+    // get and display peer info
+    char addrpeer6[BUF_SIZE];
+    getpeername(client_fd, (struct sockaddr *)&clientaddr, &addrsize);
+    inet_ntop(AF_INET6, &clientaddr.sin6_addr, addrpeer6, sizeof(addrpeer6));
+    printf("New connection - addr: %s\n", addrpeer6);
+    printf("New connection - port: %d\n", ntohs(clientaddr.sin6_port));
+
+    // add new client to epoll list
+    if (epoll_add_fd(epoll_fd, client_fd, EPOLL_EVENTS) < 0)
+	{
+        printf("Error: failed to add client fd %d to epoll\n", client_fd);
+        return;
+    }
+}
+
+void process_tun(int tun_fd)
+{
+    char* buf = bufs[tun_fd].buf;
+    int& nread = bufs[tun_fd].used;
+
+    int size;
+    // read data from tun
+    if ((size = read(tun_fd, buf+nread, BUF_SIZE-nread)) < 0)
+	{
+        printf("Error: can not read from tun\n");
+        return;
+    }
+    nread += size;
+
+    while(true)
+	{
+
+        if (nread < (int)sizeof(struct iphdr)) // linux
+            break;
+        
+        // extract ip address from ip header
+        struct iphdr hdr = *(struct iphdr *)buf;
+        int iplen = nread;
+        // if (nread < iplen) break; ??		
+
+		int id = search_user_info_by_addr(hdr.daddr);
+		if (id == -1)
+		{
+			printf("Warning: can not locate ip packet dest\n");
+			continue;
+		}
+		int client_fd = users[id].fd;
+
+		message reply;
+		reply.type = MSG_DATA_RSB;
+		reply.length = iplen + sizeof(reply);
+
+		int ret;
+		if ((ret = write_all(client_fd, &reply, sizeof(reply))) < (int)sizeof(reply))
+		{
+			printf("Error: send reply header failed\n");
+			return;
+		}
+		if ((ret = write_all(client_fd, buf, iplen)) < 0)
+		{
+			printf("Error: send reply data failed\n");
+			return;
+		}
+
+		printf("Package tunnel - length: %u\n", iplen + sizeof(message));
+
+        memmove(buf, buf + iplen, nread - iplen);
+        nread -= iplen;
+    }
+}
+
+void process_client(int client_fd, int tun_fd, int epoll_fd)
+{
+    printf("process_client\n");
+
+    char* buf = bufs[client_fd].buf;
+    int& nread = bufs[client_fd].used;
+    
+    ssize_t size;
+    // read data from client
+    size = read(client_fd, buf+nread, BUF_SIZE-nread);
+
+    if (size <= 0)
+	{
+        if (errno == ECONNRESET || size == 0) printf("Connection ternimated\n");
+        else printf("Error: error at read client:%s\n", strerror(errno));
+        free_client_fd(client_fd, epoll_fd);
+        return;
+    }
+    printf("[debug] size = %d\n", size);
+    nread += size;
+
+    // process incoming data from client
+    while(true)
+	{
+        if (nread < sizeof(message)) break;
+
+        message msg = *(message *)buf;
+        int msglen = msg.length;
+        if (nread < msg.length) break;
+
+        // process data
+        if (msg.type == MSG_IP_REQ)
+		{
+            // allocate ip address
+            int id;
+            if ((id = allocate_ip_addr(client_fd)) < 0)
+			{
+                printf("Error: ip address pool is full\n");
+                return;
+            }
+
+            // send ip response
+            char bufreply[BUF_SIZE];
+            sprintf(bufreply, "%s 0.0.0.0 %s %s %s", users[id].addr, DNS1, DNS2, DNS3);
+            int buflen = strlen(bufreply);
+
+            message reply;
+            reply.type = MSG_IP_RSB;
+            reply.length = buflen + sizeof(message);
+            if (write_all(client_fd, &reply, sizeof(reply)) < (int)sizeof(reply))
+			{
+                printf("Error: send reply header failed\n");
+                return;
+            }
+            if (write_all(client_fd, bufreply, buflen) < buflen)
+			{
+                printf("Error: send reply data failed\n");
+                return;
+            }
+        }
+        else if (msg.type == MSG_DATA_REQ)
+		{
+            // send data to tun
+            int datalen = msglen - sizeof(message);
+            if (write_all(tun_fd, buf+sizeof(message), datalen) < datalen)
+			{
+                printf("Error: send data to tun failed\n");
+                return;
+            }
+        }
+        else if (msg.type == MSG_HEARTBEAT)
+		{
+            printf("client keep-alive\n");
+
+            int id = search_user_info_by_fd(client_fd);
+            users[id].last_heartbeat_recved_secs = time(0);
+        }
+        else
+		{
+            printf("Warning: unknown type of data from client fd %d\n", client_fd);
+        }
+
+        printf("Package client - length: %d\n", msglen);
+        memmove(buf, buf + msglen, nread - msglen);
+        nread -= msglen;
+    }
+}
+
+void process_heartbeat(int epoll_fd)
+{
+    for(int i = 0; i < CLIENT_NUM; i ++)
+        if (users[i].fd >= 0 && !users[i].status)
+		{
+            time_t now = time(0);
+            if (now - users[i].last_heartbeat_sent_secs > HEARTBEAT_INTERVAL)
+			{
+                message heartbeat;
+                heartbeat.type = MSG_HEARTBEAT;
+                heartbeat.length = sizeof(message);
+                if (write_all(users[i].fd, &heartbeat, sizeof(message)) != sizeof(message))
+				{
+                    printf("Error: fail to send heartbeat packet\n");
+                }
+                users[i].last_heartbeat_sent_secs = now;
+            }
+            if (now - users[i].last_heartbeat_recved_secs > HEARTBEAT_TIMEOUT)
+			{
+                free_client_fd(users[i].fd, epoll_fd);
+            }
+        }
+}
+
+int main()
+{	
+	if (MainThread() == -1) goto clean;
+	for (;;)
+	{
+		// wait for all epoll events
+		int num_events;
+		struct epoll_event events[EPOLL_MAX_EVENTS];
+		if ((num_events = epoll_wait(epoll_fd, events, EPOLL_MAX_EVENTS, 1)) < 0)
+		{
+			printf("Error: failed to wait for epoll events\n");
+			goto clean;
+		}
+
+		// handle each event
+		int i;
+        for (i = 0; i < num_events; i++)
+		{
+            // event from server socket
+            if (events[i].data.fd == server_fd) process_server(server_fd, epoll_fd);
+            // event from tun
+            else if (events[i].data.fd == tun_fd) process_tun(tun_fd);
+            // event from client socket
+            else process_client(events[i].data.fd, tun_fd, epoll_fd);
+        }
+
+        process_heartbeat(epoll_fd);
+
+        fflush(stdout);
 	}
 
 clean:
--- server/server_util.h
+++ server/server_util.h
@@ -24,18 +24,26 @@
 #define SERV_PORT           6666
 #define MAX_FDS             2048
 #define ADDRESS_PREFIX      "13.8.0"
+#define NETMASK             "255.255.255.0"
+#define DNS1                "166.111.8.28"
+#define DNS2                "166.111.8.29"
+#define DNS3                "8.8.8.8"
 #define CLIENT_NUM          256
 #define EPOLL_MAX_EVENTS    2048
 #define BUF_SIZE            65536
 #define EPOLL_EVENTS        (EPOLLIN|EPOLLERR|EPOLLHUP)
+#define HEARTBEAT_INTERVAL  20
+#define HEARTBEAT_TIMEOUT   60
 
 typedef struct User_Info_Table 		//客户信息表
 {
 	char addr[32];		            //IP地址
 	int status;			            //标志位
 	int fd; 						//套接字描述符
-	int count;						//标志位
-	unsigned long int secs;			//上次收到keeplive时间
+
+	time_t last_heartbeat_sent_secs;
+    time_t last_heartbeat_recved_secs;
+	
 	struct in_addr v4addr;			//服务器给客户端分配的IPV4地址
 	struct in6_addr v6addr;			//客户端的IPV6地址
 	User_Info_Table() {
@@ -47,5 +55,11 @@ typedef struct buffer_entry {
     char buf[BUF_SIZE];
     int used;
 } buffer_entry;
+
+// message between server and client
+typedef struct message {
+    int length;
+    char type;
+} __attribute__((packed)) message;
 
 #endif
